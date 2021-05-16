819915842deff52782c99841f755bc7dcfa8be36
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2021-05-16 03:15:20 -0400

    presentation setup

---
+++ presentation/src/slides/slide1.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Slide, Heading, FlexBox} from 'spectacle'
+
+export default function Slide1(){
+    return(
+        <Slide>
+            <FlexBox height="100%">
+                <Heading>Intro to Ethers.js</Heading>
+            </FlexBox>
+        </Slide>
+    )
+}
