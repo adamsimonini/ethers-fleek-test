ddabac31381750e1367f5a1bf09d0c6ccf5647bb
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2021-05-16 02:35:28 -0400

    project setup

---
+++ presentation/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {Deck} from 'spectacle'
+import Slide1 from './slides/slide1';
+
+function App() {
+  return (
+    // <div className="App">
+      <Deck>
+        <Slide1 />
+      </Deck>
+    // </div>
+  );
+}
+
+export default App;
---
+++ presentation/src/slides/slide1.jsx
@@ -0,0 +1,10 @@
+import {Slide, Heading} from 'spectacle'
+import React from 'react'
+
+export default function Slide1(){
+    return(
+        <Slide>
+            <Heading>Intro to Ethers.js</Heading>
+        </Slide>
+    )
+}
