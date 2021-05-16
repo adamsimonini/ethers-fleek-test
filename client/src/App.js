c5d2e78fdcfc5c40083c7c5a05ae3f69b809b675
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2021-05-16 18:52:21 -0400

    added ethers provider with infura

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar'
 import List from './components/List'
-
+import {ethers} from 'ethers'
 
 function App() {
   // let array = []
+
+  const provider = new ethers.providers.InfuraProvider( 4, 'f6268155b09c4c5f87492dc2e5de22a0')
+  console.log(provider)
+
   return (
     <div className="App">
       <Navbar />
