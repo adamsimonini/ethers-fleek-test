69301521107806f032c7ff751c12dddf4d734ae2
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2021-05-16 23:16:00 -0400

    added build script with deplloyment settings for rinkeby

--- smart-contract/hardhat.config.js
+++ smart-contract/hardhat.config.js
@@ -1,7 +1,37 @@
 require('@nomiclabs/hardhat-waffle')
+require('dotenv').config()
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
 module.exports = {
-  solidity: "0.8.3",
+
+  defaultNetwork: 'rinkeby',
+
+  solidity: {
+    version: '0.8.0',
+    settings: {
+      optimizer: {
+        enabled:true,
+        runs: 200
+      }
+    }
+  },
+  networks: {
+    hardhat: {
+    },
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/5OmoYEM6IxrGBKSxMt0cjjmlmQ9S9EDI',
+      accounts: [`0x${process.env.PVT_KEY}`]
+    }
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  mocha: {
+    timeout: 20000
+  }
 };
