c627835afe4cc7f4623e3345574cbece8cf5723f
Pradhumna Pancholi <pnpancholi6629.pnp@gmail.com>
2021-05-17 20:45:49 -0400

    reset for start version

--- client/src/App.js
+++ client/src/App.js
@@ -2,31 +2,16 @@ import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar'
 import List from './components/List'
-import {ethers} from 'ethers'
-import abi from './data/abi.json'
+// import abi from './data/abi.json'
 
 function App() {
   
-  const [workshops, setWorkshops] = React.useState([])
-
-  React.useEffect(() => {
-    fetchWorkshopData()
-  }, [])
-
-  const fetchWorkshopData = async () => { 
-    const provider = new ethers.providers.InfuraProvider( 4, 'f6268155b09c4c5f87492dc2e5de22a0')
-    const contractAddr = '0xC4b3fD5dB1BcDeb8CB56E6530c52F2ae5265dAE0'
-    const contract = new ethers.Contract(contractAddr, abi ,provider)
-    console.log(contract)
-    const workshops = await contract.getWorkshops() 
-    console.log(workshops)
-    setWorkshops(workshops)
-  }
+  // const [workshops, setWorkshops] = React.useState([])
 
   return (
     <div className="App">
       <Navbar />
-      <List listData={workshops} />
+      <List />
     </div>
   );
 }
